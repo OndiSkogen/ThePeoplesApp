dca64db3c8615b093d5d5bf19438827a0c1f26cf
OndiSkogen <fiskkneipp@gmail.com>
2018-12-13 18:02:08 +0100

    hej hopp

--- ModelAndSuch/Models/Entities/Person.cs
+++ ModelAndSuch/Models/Entities/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ModelAndSuch.Models.Entities
 {
@@ -11,7 +12,15 @@ namespace ModelAndSuch.Models.Entities
         }
 
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter a name")]
         public string Name { get; set; }
+
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "Enter a valid email address")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        [HardCodedEmail("acme.com")]
         public string Email { get; set; }
 
         public ICollection<Company> Company { get; set; }
